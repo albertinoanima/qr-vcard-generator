140a0e150f65a9c5822486986b9139c4ecd4d4da
Albertino Augusto <105373514+albertinoanima@users.noreply.github.com>
2023-10-12 11:49:07 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,64 +1,107 @@
 const formInline = $('.form-inline');
 formInline.on('input', () => {
-  const firstName = $('#firstName').val();
-  const lastName = $('#lastName').val();
-  const fnVal = 'FN%3A' + firstName + '+' + lastName + '%0A';
-  const nVal = 'N%3A' + lastName + '%3B' + firstName + '%0A';
+  const name = $('#firstName').val();
+  const surname = $('#lastName').val();
+  const jobTitle = $('#posTitle').val();
+  const company = $('#coName').val();
+  const personalPhone = $('#phonePersonal').val();
+  const businessPhone = $('#phoneBusiness').val();
+  const personalEmail = $('#emailPersonal').val();
+  const professionalEmail = $('#emailBusiness').val();
 
-  const posTitle = $('#posTitle').val();
-  const posTitleVal = 'TITLE%3A' + posTitle + '%0A';
-
-  const coName = $('#coName').val();
-  const coNameVal = 'ORG%3A' + coName + '%0A';
-
-  const phonePersonal = $('#phonePersonal').val();
-  const phonePersonalVal = 'TEL%3BHOME%3BVOICE%3A' + phonePersonal + '%0A';
-
-  const phoneBusiness = $('#phoneBusiness').val();
-  const phoneBusinessVal = 'TEL%3BWORK%3BVOICE%3A' + phoneBusiness + '%0A';
-
-  const emailPersonal = $('#emailPersonal').val();
-  const emailPersonalVal = 'EMAIL%3BHOME%3BINTERNET%3A' + emailPersonal + '%0A';
-
-  const emailBusiness = $('#emailBusiness').val();
-  const emailBusinessVal = 'EMAIL%3BWORK%3BINTERNET%3A' + emailBusiness + '%0A';
-
-  const website = $('#website').val();
-  const websiteVal = 'URL%3A' + website + '%0A';
-
-  const addyStreet = $('#addyStreet').val();
-  const addyStreetVal = addyStreet + '%3B';
-
-
-  const addyCity = $('#addyCity').val();
-  const addyCityVal = addyCity + '%3B';
-  const addyState = $('#addyState').val();
-  const addyStateVal = addyState + '%3B';
-  const addyZip = $('#addyZip').val();
-  const addyZipVal = addyZip + '%3B';
-  const addyCountry = $('#addyCountry').val();
-  const adrVal = 'ADR%3A%3B%3B' + addyStreetVal + addyCityVal + addyStateVal + addyZipVal + addyCountry + '%0A';
-
-  
-  const qrUrlStart = 'https://api.qrserver.com/v1/create-qr-code/?data=BEGIN%3AVCARD%0AVERSION%3A2.1%0A';
-  const qrUrlEnd = 'END%3AVCARD%0A';
-  const qrURL = qrUrlStart + fnVal + nVal + posTitleVal + phoneBusinessVal + phonePersonalVal + emailPersonalVal + emailBusinessVal + websiteVal + adrVal + coNameVal + qrUrlEnd;
-  $('.qr-img').attr('src', qrURL);
-  $('.biscard-out').html(`
+  const biscardOut = document.getElementById("biscard-out");
+  biscardOut.innerHTML = `
     <div class="biscard-img">
-      <h3>${firstName + ' ' + lastName}</h3>
-      <div>${posTitle + ' | ' + coName}</div>
+      <h3>${name + ' ' + surname}</h3>
+      <div>${jobTitle + ' | ' + company}</div>
       <div class="phone">
-        w: <a href="tel:+1-${phoneBusiness}">${phoneBusiness}</a> | 
-        c: <a href="tel:+1-${phonePersonal}">${phonePersonal}</a>
+        w: <a href="tel:+1-${businessPhone}">${businessPhone}</a> | 
+        c: <a href="tel:+1-${personalPhone}">${personalPhone}</a>
       </div>
       <div class="email">
-        <a href="mailto:${emailPersonal}">
-          ${emailPersonal}
+        <a href="mailto:${personalEmail}">
+          ${personalEmail}
         </a> | 
-        <a href="mailto:${emailBusiness}">
-          ${emailBusiness}
+        <a href="mailto:${professionalEmail}">
+          ${professionalEmail}
         </a> 
       </div>
-    </div>`);
-})
+    </div>`;
+});
+
+function excuteInstruction() {
+  const name              = String($('#firstName').val());
+  const surname           = String($('#lastName').val());
+  const jobTitle          = String($('#posTitle').val());
+  const company           = String($('#coName').val());
+  const personalPhone     = String($('#phonePersonal').val());
+  const businessPhone     = String($('#phoneBusiness').val());
+  const personalEmail     = String($('#emailPersonal').val());
+  const professionalEmail = String($('#emailBusiness').val());
+  const website           = String($('#website').val());
+
+  const street            = String($('#addyStreet').val());
+  const city              = String($('#addyCity').val());
+  const country           = String($('#addyCountry').val());
+
+  const encondedStringData = encodeURIComponent(generateVCardString(
+    name,
+    surname,
+    jobTitle,
+    company,
+    personalPhone,
+    businessPhone,
+    personalEmail,
+    professionalEmail,
+    website,
+    city,
+    country,
+    street
+  ));
+
+  const qrImg = document.getElementById("qr-img");
+  qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?data=${encondedStringData}`;
+};
+
+// Create a function that generates a VCard string.
+function generateVCardString(name, surname, jobTitle, company, personalPhone, businessPhone, personalEmail, professionalEmail, website, city, country, street) {
+  const vcardString = `BEGIN:VCARD
+VERSION:3.0
+N:${surname};${name}
+FN:${surname} ${name}
+TITLE:${jobTitle}
+ORG:${company}
+TEL;TYPE=VOICE;HOME:${personalPhone}
+TEL;TYPE=VOICE;WORK:${businessPhone}
+EMAIL;TYPE=HOME:${personalEmail}
+EMAIL;TYPE=WORK:${professionalEmail}
+URL:${website}
+ADR;TYPE=WORK:;;${street};${city};${country}
+END:VCARD`;
+  return vcardString;
+}
+
+/*
+// Convert the special characters to UTF-8.
+const encodedString = encodeURIComponent(generateVCardString(
+  'João',
+  'Da Graça',
+  'Função',
+  'Anima',
+  '+258848131298',
+  '+258848131298',
+  'albertinoaugusto@anima.co.mz',
+  'albertinoaugusto@anima.co.mz',
+  'https://albertinoaugusto.co.mz',
+  'Mountain View',
+  'CA',
+  '94043',
+  'USA',
+  '123 Main Street'
+));
+
+setTimeout(() => {
+  const qrImg = document.getElementById("qr-img");
+  qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?data=${encodedString}`;
+}, 2000)
+*/
